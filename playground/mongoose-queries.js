8f3a744aa6f79de83f351cb29f70666796da86ef
gyGil <geunyoungGil@gmail.com>
2018-07-13 16:48:23 -0400

    Id validation

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,27 @@
+const {ObjectId} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '5b490c91e37b22327fb95f29';
+
+if (!ObjectId.isValid(id)) {
+    console.log('ID not valid');
+}
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    console.log('Todo by Id', todo);
+}).catch((e) => console.log(e));
