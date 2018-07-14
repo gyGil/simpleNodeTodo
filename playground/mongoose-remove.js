0e37ecaaa02dd2b6b81b154ba5853b8884f5b512
gyGil <geunyoungGil@gmail.com>
2018-07-13 20:37:25 -0400

    Setup seperate dev and test

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,12 @@
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+Todo.remove({}).then((result) => {
+    console.log(result);
+});
+
+Todo.findByIdAndRemove('').then((todo) => {
+    console.log(todo);
+});
